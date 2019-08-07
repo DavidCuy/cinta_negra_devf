2fc452846270f793dc02db250430c10edd0057c3
David Cuy <david.cuy.sanchez@gmail.com>
2019-08-06 20:40:28 -0500

    Componentes principales

--- my-blog/src/app/app.module.ts
+++ my-blog/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { PostComponent } from './components/post/post.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    PostsComponent,
+    PostComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
